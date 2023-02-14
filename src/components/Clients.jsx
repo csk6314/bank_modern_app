86e423f192534f3cd38ba51a3a16063c21de89d1
csk6314 <37896060+csk6314@users.noreply.github.com>
2023-02-14 21:57:03 +0900

    testimonial section

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { clients } from "../constants";
+import styles from "../styles";
 
-const Clients = () => {
-  return <div>Clients</div>;
-};
+const Clients = () => (
+  <section className={`${styles.flexCenter} my-4`}>
+    <div className={`${styles.flexCenter} w-full flex-wrap`}>
+      {clients.map((client) => (
+        <div
+          key={client.id}
+          className={`flex-1 ${styles.flexCenter}
+          min-w-[120px] 
+          sm:min-w-[192px]`}
+        >
+          <img
+            src={client.logo}
+            alt="client"
+            className="client__glow w-[100px] object-contain sm:w-[192px]"
+          />
+        </div>
+      ))}
+    </div>
+  </section>
+);
 
 export default Clients;
--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,7 +1,39 @@
 import React from "react";
+import styles from "../styles";
+import { feedback } from "../constants";
+import FeedbackCard from "./FeedbackCard";
+const Testimonials = () => (
+  <section
+    id="clients"
+    className={`${styles.paddingY} ${styles.flexCenter} relative flex-col`}
+  >
+    {/* todo*/}
+    <div className=" blue__gradient absolute -right-[50%] z-[0] h-[60%] w-[60%] rounded-full" />
 
-const Testimonials = () => {
-  return <div>Testimonials</div>;
-};
+    <div
+      className="relative z-[1] mb-6 flex
+  w-full flex-col items-center justify-between sm:mb-16 md:flex-row"
+    >
+      <h1 className={`${styles.heading2}`}>
+        What people are <br className="hidden sm:block" />
+        saying about us
+      </h1>
+      <div className="mt-6 w-full md:mt-0">
+        <p className={`${styles.paragraph} max-w-[450px] text-left`}>
+          Everything you need to accept card payments and grow up your business
+          anywhere on the planet.
+        </p>
+      </div>
+    </div>
+    <div
+      className="feedback-container relative z-[1] flex w-full flex-wrap
+    justify-center sm:justify-start"
+    >
+      {feedback.map((card) => (
+        <FeedbackCard key={card.id} {...card} />
+      ))}
+    </div>
+  </section>
+);
 
 export default Testimonials;
