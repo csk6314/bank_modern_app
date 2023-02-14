575a7c0d31336a8a4a70f6e466c1a2164d40a614
csk6314 <37896060+csk6314@users.noreply.github.com>
2023-02-14 17:36:24 +0900

    business section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
-const Button = () => {
-  return <div>Button</div>;
+const Button = ({ styles, text }) => {
+  return (
+    <button
+      type="button"
+      className={`bg-blue-gradient py-4 px-6 font-poppins text-[18px] font-medium text-primary outline-none ${styles}`}
+    >
+      {text}
+    </button>
+  );
 };
 
 export default Button;
